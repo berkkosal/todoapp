95d879e37a5db6127d35fb905a1b1692f649240e
berkkosal <kosalnberk@gmail.com>
2022-10-19 18:55:15 +0300

    database connection kuruldu

--- src/main/java/com/berk/todoapp/controller/DatabaseAccessController.java
+++ src/main/java/com/berk/todoapp/controller/DatabaseAccessController.java
@@ -1,4 +1,44 @@
 package com.berk.todoapp.controller;
 
+import javafx.scene.chart.PieChart;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class DatabaseAccessController {
+    //***************************Singeleton***************************//
+    private static DatabaseAccessController instance;
+    private DatabaseAccessController() {
+        init();
+    }
+    public static DatabaseAccessController getInstance() {
+        if (instance == null) {
+            instance = new DatabaseAccessController();
+        }
+        return instance;
+    }
+
+    //***************************Değişkenler***************************//
+    private final String dataSource = "jdbc:postgresql://localhost:5432/postgres";
+    private final String dataUsername = "postgres";
+    private final String dataPassword = "123";
+    private Connection connection = null;
+    private Statement statement = null;
+
+
+//select todo,iscompleted from todo where userid = (select id from users where email = 'berkko@gmail.com');
+
+
+    private void init() {
+        try {
+            //Bağlantı nesnesi
+            connection = DriverManager.getConnection(dataSource, dataUsername, dataPassword);
+            System.out.println("Connected to the database");
+            //Baglanti nesnesi uzerinden islem yapmaya yarayan nesne "Statement"
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.berk.todoapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.berk.todoapp to javafx.fxml;
