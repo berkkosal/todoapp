8559297c759a2538e0c3f8479cb65224d585242d
berkkosal <kosalnberk@gmail.com>
2022-10-20 18:41:19 +0300

    properties oluşturuldu, host authenticationı eklendi

--- src/main/java/com/berk/todoapp/controller/MailSenderController.java
+++ src/main/java/com/berk/todoapp/controller/MailSenderController.java
@@ -1,15 +1,33 @@
 package com.berk.todoapp.controller;
 
-import java.util.*;
-import javax.mail.*;
-import javax.mail.internet.*;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
-import javax.activation.*;
-import javax.mail.Transport;
+import java.util.Properties;
 
 public class MailSenderController {
-    
 
+    public static void sendMail(String recepient) {
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        String hostEmail = "";
+        String hostMailPass = "";
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(hostEmail,hostMailPass);
+            }
+        });
+
+
+        
+    }
 
 }
 
