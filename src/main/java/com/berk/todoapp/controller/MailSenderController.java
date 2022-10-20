dfad2d537d8d505b0ed14d452293f8e89c34cdeb
berkkosal <kosalnberk@gmail.com>
2022-10-20 19:07:09 +0300

    mailsender metotları yazıldı

--- src/main/java/com/berk/todoapp/controller/MailSenderController.java
+++ src/main/java/com/berk/todoapp/controller/MailSenderController.java
@@ -1,13 +1,14 @@
 package com.berk.todoapp.controller;
 
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class MailSenderController {
 
-    public static void sendMail(String recepient) {
+    public static void sendMail(String recepient) throws MessagingException {
+        System.out.println("Mail göndermeye hazırlanıyor.");
         Properties properties = new Properties();
 
         properties.put("mail.smtp.auth", "true");
@@ -15,18 +16,35 @@ public class MailSenderController {
         properties.put("mail.smtp.host", "smtp.gmail.com");
         properties.put("mail.smtp.port", "587");
 
-        String hostEmail = "";
-        String hostMailPass = "";
+        String hostEmail = "Javamaildenemem@gmail.com";
+        String hostMailPass = "Java123456789";
 
         Session session = Session.getInstance(properties, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(hostEmail,hostMailPass);
+                return new PasswordAuthentication(hostEmail, hostMailPass);
             }
         });
 
+        Message message = prepareMessage(session, hostEmail, recepient);
+        Transport.send(message);
+        System.out.println("Mail gönderildi.");
 
-        
+
+    }
+
+    private static Message prepareMessage(Session session, String hostEmail, String recepient) {
+        Message message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress(hostEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setSubject("Konu bir deneme");
+            message.setText("Deneme içerikli yazı.\n Nasıl harika değil mi?");
+            return message;
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
---
+++ src/main/java/com/berk/todoapp/controller/MailSenderDenemeMain.java
@@ -0,0 +1,8 @@
+package com.berk.todoapp.controller;
+
+public class MailSenderDenemeMain {
+
+    public static void main(String[] args) {
+
+    }
+}
